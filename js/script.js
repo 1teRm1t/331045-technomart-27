97bf42cf1e09bb30e2ead30bb02d0a9896938032
Сергей <smeznikov@yandex.ru>
2020-03-04 00:01:05 +0300

    Добавлен js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,71 @@
-var link = document.querySelector(".js-popup-link")
+var link = document.querySelector(".js-popup-link");
 
-var popup = document.querySelector(".modal-feedback")
-var close = popup.querySelector(".modal-close")
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
 
-link.addEventListener("click", function(evt) {
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
 });
 
-close.addEventListener("click", function(evt) {
+close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
 });
 
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+  } else {
+    localStorage.setItem("login", login.value)
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
 
 
-var mapLink = document.querySelector(".js-map-link")
 
-var mapPopup = document.querySelector(".modal-map")
-var mapClose = mapPopup.querySelector(".modal-close")
+var mapLink = document.querySelector(".js-map-link");
 
-mapLink.addEventListener("click", function(evt) {
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.add("modal-show");
 });
 
-mapClose.addEventListener("click", function(evt) {
+mapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.remove("modal-show");
 });
